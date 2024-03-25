7edd18199e814a53194bc37698491e90b7b36bd6
Max12243 <laurin.maxime1@gmail.com>
2024-03-25 16:44:57 -0400

    enlever html

--- controlleurs/pokemon.controllers.js
+++ controlleurs/pokemon.controllers.js
@@ -8,22 +8,7 @@ module.exports = {
     afficherTousPokemon: (req, res) => {
         model.obtenirTousPokemonDB()
             .then(result => {
-                // Convertir les résultats en une chaîne de caractères avec un retour à la ligne entre chaque entrée
-                const formattedResult = result.map(pokemon => `
-                    <div>
-                        <p><strong>Nom:</strong> ${pokemon.nom} --- <strong>ID:</strong> ${pokemon.id}</p>
-                        <p><strong>Type primaire:</strong> ${pokemon.type_primaire} / ${pokemon.type_secondaire ? pokemon.type_secondaire : 'N/A'} </p>
-                        <tr>
-                            <td><p><strong>PV:</strong> ${pokemon.pv}</p></td>
-                            <td><p><strong>Attaque:</strong> ${pokemon.attaque}</p></td>
-                            <td><p><strong>Défense:</strong> ${pokemon.defense}</p></td>
-                        </tr>
-                    </div>
-                    <hr>
-                `).join('');
-
-                // Renvoyer la chaîne formatée
-                res.send(formattedResult);
+                res.send(result);
             })
             .catch(error => {
                 console.error('Erreur lors de la récupération des pokémons :', error);
